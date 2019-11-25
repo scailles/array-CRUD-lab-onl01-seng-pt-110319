69cef243cd558c22be1b8d4a2777c2ff6442bd3e
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-25 06:35:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array=["I]
+  array=["I","am","really","learning"]
+  
 end
 
 def remove_element_from_end_of_array(array)
