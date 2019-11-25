46246a64596dbd0fd5e834cd9052d9418a14e733
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-25 06:40:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array=["wow",I","am","really","learning","arrrays!""]
+  array=["wow","I","am","really","learning","arrrays!"]
   array.pop
 end
 
