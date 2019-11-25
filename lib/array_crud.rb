4f61896f0ef74aa8781698c00689208e8683c6d5
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-25 06:32:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- #{create_an_array} << "five"
+ array=["one","two","three","four"]
+ array<<"five"
 end
 
 def add_element_to_start_of_array(array, element)
